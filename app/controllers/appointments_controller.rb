f431dd514a0eccd39484b386c3d3fdc819d6c6f6
brianabaker <brianaclairbaker@gmail.com>
2018-01-24 12:57:58 -0500

    working on appointments

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -3,34 +3,61 @@
 class AppointmentsController < ApplicationController
 
   def index
+    @appointments = Appointment.all
+    @user = User.find_by(id: session[:user_id])
+    @your_appointments = @appointments.select{|appointment| appointment.sitter_id == @user.id }
   end
+
 
   def new
     @appointment = Appointment.new
     @user = User.find_by(id: params[:user_id])
     @pet = Pet.find_by(id: params[:pet_id])
-
   end
 
   def create
-    # byebug
-    @appointment = Appointment.new(sitter_id: params[:user_id], cuddle_buddy_id: params[:pet_id], checkin: params[:checkin], checkout: params[:checkout])
+    @appointment = Appointment.new(appointment_params)
     if @appointment.valid?
       @appointment.save
-      redirect_to user_appointment_path
+
+      redirect_to appointment_path(@appointment)
 
     else
       flash[:error] = @appointment.errors.full_messages
-      redirect_to user_new_appointment_path
+      redirect_to new_appointment_path
 
     end
   end
 
+  def show
+    @appointment = Appointment.find(params[:id])
+  end
+
+  def edit
+    @appointment = Appointment.find(params[:id])
+    @pet = @appointment.cuddle_buddy
+    @user = User.find_by(id: session[:user_id])
+    # @pet = Pet.find(params[:id])
+  end
+
+  def update
+    @appointment = Appointment.find(params[:id])
+    @appointment.update(appointment_params)
+    redirect_to appointment_path(@appointment)
+
+  end
+
+  def destroy
+    @appointment = Appointment.find(params[:id])
+    @appointment.destroy
+    redirect_to appointments_path
+  end
+
   private
 
-  # def appointment_params
-  #   params.require(:appointment).permit(:user_id, :pet_id, :checkin, :checkout)
-  # end
+  def appointment_params
+    params.require(:appointment).permit(:sitter_id, :cuddle_buddy_id, :checkin, :checkout)
+  end
 
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -14,24 +14,26 @@ Rails.application.routes.draw do
 
   resources :pets, only: [:index, :show]
 
-  resources :users, only: [:new, :create, :show, :edit, :update, :destroy]  do
-    get "/pets", to: 'users#pets'
+  post '/appointments/new', to: 'appointments#new', as: 'trial'
 
-    get '/pets/:id/edit', to: 'pets#edit', as: "edit_pet"
-    get '/pets/:pet_id/appointments/new', to: 'appointments#new', as: "new_appointment"
-    post '/pets/:pet_id/appointments/new', to: 'appointments#create', as: "create_appointment"
-    get '/appointments', to: 'appointments#show', as: 'appointment'
+  resources :users, only: [:new, :create, :show, :edit, :update, :destroy]  do
+    # get "/pets", to: 'users#pets'
+    #
+    # get '/pets/:id/edit', to: 'pets#edit', as: "edit_pet"
+    # get '/pets/:pet_id/appointments/new', to: 'appointments#new', as: "new_appointment"
+    # post '/pets/:pet_id/appointments/new', to: 'appointments#create', as: "create_appointment"
+    # get '/appointments', to: 'appointments#show', as: 'appointment'
     # resources :appointments
 
-    resources :pets, only: [:new, :create, :update, :destroy]
+    resources :pets, shallow: true, only: [:new, :create, :update, :destroy]
     # get '/pets/new', to: 'pets#new'
     # get '/pets/:id', to: 'pets#show', as: 'show_pet'
     # get '/pets/:id/edit', to: 'pets#edit', as: 'pet'
     # post '/pets', to: 'pets#create'
     # patch '/pets/:id/edit', to: 'pets#update', as: 'edit_pet'
     # delete '/pets/:id', to: 'pets#destroy'
-  end
 
-  resources :sessions, only: [:new, :create, :destroy]
-
+end
+resources :appointments
+resources :sessions, only: [:new, :create, :destroy]
 end
