e1fa14aa06a50a22a74ea54dca672295e9e32a6d
brianabaker <brianaclairbaker@gmail.com>
2018-01-26 10:44:45 -0500

    99% done

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,6 @@ Rails.application.routes.draw do
   root   'static_pages#home'
    get    '/help',    to: 'static_pages#help'
    get    '/about',   to: 'static_pages#about'
-   get    '/contact', to: 'static_pages#contact'
    get    '/analytics', to: 'static_pages#analytics'
    get    '/signup',  to: 'users#new'
    get    '/login',   to: 'sessions#new'
@@ -12,33 +11,13 @@ Rails.application.routes.draw do
    delete '/logout',  to: 'sessions#destroy'
 
   post '/appointments/new', to: 'appointments#new'
-
   resources :pets, only: [:show, :index]
-
   resources :appointments
   resources :sessions, only: [:new, :create, :destroy]
-
-
   resources :users, only: [:new, :create, :show, :edit, :update, :destroy]  do
     get "/pets", to: 'users#pets'
     get '/pets/:id/edit', to: 'pets#edit', as: "edit_pet"
     get '/pets/appointments', to: 'pets#appointments', as: "pets_appointments"
-    # get "/appointments", to: 'users#appointments'
-    resources :pets, shallow: true, only: [:new, :create, :update, :destroy]
+    resources :pets, shallow: true, only: [:new, :create, :edit, :update, :destroy]
   end
-
 end
-
-   # post '/reroute', to: 'application#reroute'
-
-# get '/pets/:pet_id/appointments/new', to: 'appointments#new', as: "new_appointment"
-# post '/pets/:pet_id/appointments/new', to: 'appointments#create', as: "create_appointment"
-# get '/appointments', to: 'appointments#show', as: 'appointment'
-# resources :appointments
-
-# get '/pets/new', to: 'pets#new'
-# get '/pets/:id', to: 'pets#show', as: 'show_pet'
-# get '/pets/:id/edit', to: 'pets#edit', as: 'pet'
-# post '/pets', to: 'pets#create'
-# patch '/pets/:id/edit', to: 'pets#update', as: 'edit_pet'
-# delete '/pets/:id', to: 'pets#destroy'
